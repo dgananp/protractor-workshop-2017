b1ef9f431eb1a7c322f40d938858e00eccab0748
Duvan Gañan <duvanganan@MacBook-Pro-de-Duvan.local>
2017-10-11 16:47:12 -0500

    third correction explicit-waits

--- src/page/MenuContent.page.ts
+++ src/page/MenuContent.page.ts
@@ -1,12 +1,12 @@
 import { $, ElementFinder, promise, ExpectedConditions, browser } from 'protractor';
 
 export class MenuContentPage {
- private get tShirtMenu(): ElementFinder {  
+ private get tShirtMenu(): ElementFinder {
   return $('#block_top_menu > ul > li:nth-child(3) > a');
  }
 
  public goToTShirtMenu(): promise.Promise<void> {
-  browser.wait(ExpectedConditions.elementToBeClickable(this.tShirtMenu));
+   browser.wait(ExpectedConditions.elementToBeClickable(this.tShirtMenu));
    return this.tShirtMenu.click();
  }
 }
--- src/page/ProductAddedModal.page.ts
+++ src/page/ProductAddedModal.page.ts
@@ -1,12 +1,12 @@
 import { $, ElementFinder, promise, browser, ExpectedConditions} from 'protractor';
 
 export class ProductAddedModalPage {
- private get proccedToCheckoutButton(): ElementFinder {  
-  return $('[style*="display: block;"] .button-container > a');
+ private get proccedToCheckoutButton(): ElementFinder {
+   return $('[style*="display: block;"] .button-container > a');
  }
 
  public goToSummaryStep(): promise.Promise<void> {
-   browser.wait(ExpectedConditions.elementToBeClickable(this.proccedToCheckoutButton), 5000, 'Element not clickable');
+   browser.wait(ExpectedConditions.elementToBeClickable(this.proccedToCheckoutButton), 5000);
    return this.proccedToCheckoutButton.click();
  }
 }
