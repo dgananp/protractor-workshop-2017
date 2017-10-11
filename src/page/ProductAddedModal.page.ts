6e0bec71352ddff930ff9a66840629cfe60985f1
Duvan Gañan <duvanganan@MacBook-Pro-de-Duvan.local>
2017-10-11 16:29:12 -0500

    second  correction explicit-wait

--- src/page/ProductAddedModal.page.ts
+++ src/page/ProductAddedModal.page.ts
@@ -1,12 +1,12 @@
 import { $, ElementFinder, promise, browser, ExpectedConditions} from 'protractor';
 
 export class ProductAddedModalPage {
- private get proccedToCheckoutButton(): ElementFinder {
-  browser.wait(ExpectedConditions.elementToBeClickable($('[style*="display: block;"] .button-container > a')), 5000, 'Element not clickable');
+ private get proccedToCheckoutButton(): ElementFinder {  
   return $('[style*="display: block;"] .button-container > a');
  }
 
  public goToSummaryStep(): promise.Promise<void> {
+   browser.wait(ExpectedConditions.elementToBeClickable(this.proccedToCheckoutButton), 5000, 'Element not clickable');
    return this.proccedToCheckoutButton.click();
  }
 }
