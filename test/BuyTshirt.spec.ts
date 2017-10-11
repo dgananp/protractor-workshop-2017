16d1caf51fea62dcea960b35c1674cfa1017d904
Duvan Gañan <duvanganan@MacBook-Pro-de-Duvan.local>
2017-10-11 12:09:43 -0500

    implied-wait

--- test/BuyTshirt.spec.ts
+++ test/BuyTshirt.spec.ts
@@ -29,29 +29,16 @@ describe('Buy a t-shirt', () => {
  it('then should be bought a t-shirt', async () => {
     await browser.get('http://automationpractice.com/');
     await menuContentPage.goToTShirtMenu();
-    await(browser.sleep(3000));
     await productListPage.goToProducDetail();
-    await(browser.sleep(3000));
     await productDetailPage.goToProductAdded();
-    await(browser.sleep(3000));
     await productAddedModalPage.goToSummaryStep();
-    await(browser.sleep(3000));
     await summaryStepPage.goToSignInStep();
-    await(browser.sleep(3000));
-
     await signInStepPage.signIn('aperdomobo@gmail.com', 'WorkshopProtractor');
-    await(browser.sleep(3000));
-   
     await addressStepPage.goToShippingStep();
-    await(browser.sleep(3000));
- 
     await shippingStepPage.goToPaymentStep();
-    await(browser.sleep(3000));
     await paymentStepPage.goToBankPayment();
-    await(browser.sleep(3000));
     await bankPaymentPage.goToOrderResume(); 
-    await(browser.sleep(3000));
- 
+    
     await expect(orderResumePage.getOrderConfirmation())
       .toBe('Your order on My Store is complete.');
   });
