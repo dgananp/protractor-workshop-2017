c842c58992946addea046f1a481365eef2082be5
Duvan Gañan <duvanganan@MacBook-Pro-de-Duvan.local>
2017-10-11 21:10:25 -0500

    third correction describes

--- test/BuyTshirt.spec.ts
+++ test/BuyTshirt.spec.ts
@@ -25,7 +25,8 @@ describe('workflow', () => {
       const productDetailPage: ProductDetailPage = new ProductDetailPage();
       const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
       const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-     beforeAll(async () => {
+
+      beforeAll(async () => {
         await menuContentPage.goToTShirtMenu();
         await productListPage.goToProducDetail();
         await productDetailPage.goToProductAdded();
@@ -35,6 +36,7 @@ describe('workflow', () => {
 
       describe('login', () => {
         const signInStepPage: SignInStepPage = new SignInStepPage();
+        
         beforeAll(async () => {
           await signInStepPage.signIn('aperdomobo@gmail.com', 'WorkshopProtractor');
         });
@@ -42,6 +44,7 @@ describe('workflow', () => {
         describe('address', () => {
           const addressStepPage: AddressStepPage = new AddressStepPage();
           const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+          
           beforeAll(async () => {
             await addressStepPage.goToShippingStep();
             await shippingStepPage.goToPaymentStep();
@@ -51,11 +54,12 @@ describe('workflow', () => {
             const paymentStepPage: PaymentStepPage = new PaymentStepPage();
             const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
             const orderResumePage: OrderResumePage = new OrderResumePage();
+            
             beforeAll(async () => {
               await paymentStepPage.goToBankPayment();
               await bankPaymentPage.goToOrderResume(); 
             });
-            
+
             it('BankPayment', async () => {
               await expect(orderResumePage.getOrderConfirmation())
                 .toBe('Your order on My Store is complete.');
