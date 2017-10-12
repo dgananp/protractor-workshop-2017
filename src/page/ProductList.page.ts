52b1ee0b8d257a544d9543b217ae036f87a8443f
Duvan Gañan <duvanganan@MacBook-Pro-de-Duvan.local>
2017-10-12 14:21:57 -0500

    first correction element-filter

--- src/page/ProductList.page.ts
+++ src/page/ProductList.page.ts
@@ -7,16 +7,16 @@ export class ProductListPage {
 
  private get productContainerList(): ElementArrayFinder {
   return $$('.product-container');
-}
+ }
 
-private  findByProduct(productName: string): ElementFinder {
+ private  findByProduct(productName: string): ElementFinder {
   return this.productContainerList
          .filter((product: ElementFinder) =>
           product.$('.product-name')
            .getText()
             .then((text: string) => text.includes(productName)))
          .first();
-}
+ }
 
  public selectProduct(productName: string): promise.Promise<void> {
    return this.findByProduct(productName).$('img').click();
