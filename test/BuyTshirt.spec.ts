cb421408c479162c6c8865325eaf85fb4cb17599
Duvan Gañan <duvanganan@MacBook-Pro-de-Duvan.local>
2017-10-11 21:03:39 -0500

    second correction describes

--- test/BuyTshirt.spec.ts
+++ test/BuyTshirt.spec.ts
@@ -15,42 +15,47 @@ import {
 
 describe('workflow', () => {
   describe('Open browser', () => {
-    beforeAll(async function() {
+    beforeAll(async () => {
       await browser.get('http://automationpractice.com/');
     });
+
     describe('Buy a t-shirt', () => {
       const menuContentPage: MenuContentPage = new MenuContentPage();
       const productListPage: ProductListPage = new ProductListPage();
       const productDetailPage: ProductDetailPage = new ProductDetailPage();
       const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
       const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-     beforeAll(async function() {
+     beforeAll(async () => {
         await menuContentPage.goToTShirtMenu();
         await productListPage.goToProducDetail();
         await productDetailPage.goToProductAdded();
         await productAddedModalPage.goToSummaryStep();
         await summaryStepPage.goToSignInStep();
       });
+
       describe('login', () => {
         const signInStepPage: SignInStepPage = new SignInStepPage();
-        beforeAll(async function() {
+        beforeAll(async () => {
           await signInStepPage.signIn('aperdomobo@gmail.com', 'WorkshopProtractor');
         });
+
         describe('address', () => {
           const addressStepPage: AddressStepPage = new AddressStepPage();
           const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-          beforeAll(async function() {
+          beforeAll(async () => {
             await addressStepPage.goToShippingStep();
             await shippingStepPage.goToPaymentStep();
           });
+
           describe('BankPayment', () => {
             const paymentStepPage: PaymentStepPage = new PaymentStepPage();
             const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
             const orderResumePage: OrderResumePage = new OrderResumePage();
-            beforeAll(async function() {
+            beforeAll(async () => {
               await paymentStepPage.goToBankPayment();
               await bankPaymentPage.goToOrderResume(); 
             });
+            
             it('BankPayment', async () => {
               await expect(orderResumePage.getOrderConfirmation())
                 .toBe('Your order on My Store is complete.');
